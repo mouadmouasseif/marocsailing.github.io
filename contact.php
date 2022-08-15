122c5e358fea1f2acbdd764ed79bba824645b823
mouadmouasseif <84901752+mouadmouasseif@users.noreply.github.com>
2022-08-15 22:00:53 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+
+<head>
+
+    <!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Morocco Sailing</title>
+	<meta name="description" content="J'aime vous presenter des petite information sur le sport de voile,les bateaux utiliser, les clubs, les compitition ou Maroc, les moniteurs et mais un peut d'athelete.">
+	<meta name="author" content="http://www.morroccosailing.trust">
+	
+    <!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- CSS
+	================================================== -->
+  <link rel="stylesheet" href="css/zerogrid.css">
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	
+	<link rel="stylesheet" href="css/menu.css">
+	<!-- jQuery Core Javascript -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/script.js"></script>
+	
+	<!-- Owl Stylesheets -->
+    <link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
+	
+	[if lt IE 8]
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/Items/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+      </div>
+     [endif]
+    [if lt IE 9]
+		<script src="js/html5.js"></script>
+		<script src="js/css3-mediaqueries.js"></script>
+	[endif]
+	
+</head>
+
+<body>
+	<div class="wrap-body">
+		
+		<header class="main-header">
+			<div class="zerogrid">
+				<div class="row">
+					<div class="col-1-3">
+						<a class="site-branding" href="index.html">
+							<img src="images/logo.png"/>	
+						</a><!-- .site-branding -->
+					</div>
+					<div class="col-2-3">
+						<!-- Menu-main -->
+						<div id='cssmenu' class="align-right">
+						<ul>
+							   <li class="active"><a href='index.php'><span>Home</span></a></li>
+							   <li><a href='archive.php'><span>About</span></a></li>
+							   <li><a href='bateax.php'><span>Bateaux</span></a></li>
+							   <li><a href='club.php'><span>Club</span></a></li>
+							   <li><a href='coup.php'><span>Competition & Formation</span></a></li>
+							   <li><a href='shop.php'><span>Shop</span></a></li>
+							   <li class='last'><a href='contact.php'><span>Contacts</span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		
+		<!--////////////////////////////////////Container-->
+		<section id="container" class="zerogrid">
+			<div class="wrap-container">
+				<!-----------------Content-Box-------------------->
+				<section class="content-box">
+					<div class="row wrap-box"><!--Start Box-->
+					
+						
+						
+						<div class="contact-form">
+							<h3 class="t-center">Contact Form</h3>
+							<div id="contact_form">
+								<form name="form1" id="ff" method="post" action="contact.php">
+									<label class="row">
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="text" name="name" id="name" placeholder="Enter name" required="required" />
+											</div>
+										</div>
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="email" name="email" id="email" placeholder="Enter email" required="required" />
+											</div>
+										</div>
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="text" name="subject" id="subject" placeholder="Subject" required="required" />
+											</div>
+										</div>
+									</label>
+									<label class="row">
+										<div class="wrap-col">
+											<textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
+											placeholder="Message"></textarea>
+										</div>
+									</label>
+									<center><input class="button button-skin" type="submit" name="Submit" value="Envoyer"></center>
+								</form>
+							</div>
+						</div>
+					</div>
+				</section>
+				
+			</div>
+		</section>
+		
+		<!--////////////////////////////////////Footer-->
+		<footer>
+			<div class="zerogrid">
+				<div class="wrap-footer">
+					<div class="row">
+						<div class="col-1-3 col-footer-1">
+							<div class="wrap-col">
+								<h3 class="widget-title">Suivez nous sur nos pages sur les reseaux   </h3>
+								
+							</div>
+						</div>
+						<div class="col-1-3 col-footer-2">
+							<div class="wrap-col">
+								<h3 class="widget-title">Morocco Sailing</h3>
+								<ul>
+									<li><a href="tiktok.com/@moroccosailing">Tik Tok </a> NEW</li>
+									<li><a href="https://wa.me/message/FOEK45TIX3PSM1">Whatssap </a></li>
+									<li><a href="https://www.instagram.com/mar_sailing/">Instagram </a> </li>
+									<li><a href="https://twitter.com/MarocSailing">Twitter </a> </li>
+									<li><a href="marocsailing@gmail.com">Mail </a></li>
+									<li><a href="https://www.facebook.com/Morocco-Sailing-114519989921336">Facebook</a></li>
+									<li><a href="https://www.linkedin.com/in/maroc-sailing-5378b4240/">Linkedin </a> NEW</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-1-3 col-footer-3">
+							<div class="wrap-col">
+								<h3 class="widget-title"></h3>
+								<div class="row">
+									
+									<p>
+										<strong>Opening Hours:</strong>
+										<br>
+										Lundi - Vendredi: 9:00 - 18:00
+										<br>
+										Samedi  - Dimanche: 9:00 - 2:00
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-footer">
+					<div class="bottom-social">
+						<a href="https://www.facebook.com/Morocco-Sailing-114519989921336"><i class="fa fa-facebook"></i></a>
+						<a href="https://www.instagram.com/mar_sailing/"><i class="fa fa-instagram"></i></a>
+						<a href="https://twitter.com/MarocSailing"><i class="fa fa-twitter"></i></a>
+						<a href="https://www.linkedin.com/in/maroc-sailing-5378b4240/"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-youtube"></i></a>
+					</div>
+					<div class="copyright">
+						Copyright @ - Designed by <a href="https://mouadmouasseif.ga/">Mouasseif Mouad</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		<!-- Google Map -->
+	<script>
+	  var marker;
+	  var image = 'images/map-marker.png';
+      function initMap() {
+        var myLatLng = {lat: 39.79, lng: -86.14};
+
+		// Specify features and elements to define styles.
+        var styleArray = [
+          {
+            featureType: "all",
+            stylers: [
+             { saturation: -80 }
+            ]
+          },{
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+              { hue: "#000000" },
+              { saturation: 50 }
+            ]
+          },{
+            featureType: "poi.business",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+          }
+        ];
+		
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+		   // Apply the map style array to the map.
+          styles: styleArray,
+          zoom: 7
+        });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        });
+
+		// Create a marker and set its position.
+        marker = new google.maps.Marker({
+          map: map,
+		  icon: image,
+		  draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: myLatLng
+        });
+		marker.addListener('click', toggleBounce);
+      }
+	  
+	  function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7V-mAjEzzmP6PCQda8To0ZW_o3UOCVCE&callback=initMap" async defer></script>
+	
+	</div>
+</body>
+</html>
